d50888a5f3c3992556df6322bc04e98f9fbf66c0
nazarellinazar <90243780+nazarellinazar@users.noreply.github.com>
2021-10-02 11:53:38 +0300

    update weirdString

--- src/module-1/weirdString/index.js
+++ src/module-1/weirdString/index.js
@@ -1,1 +1,11 @@
-export const weirdString = (str = "") => {};
+export const weirdString = (str = "") => {
+    const res = [];
+      const arr = str.split(" ");
+      for (let i = 0; i < arr.length; i++) {
+          const start = arr[i].slice(0, -1);
+          const fin = arr[i].slice(-1)
+          res.push(start.toUpperCase() + fin.toLowerCase());
+      }
+      return res.join(" ");
+  
+  };
